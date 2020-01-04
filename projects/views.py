4faf6797ebcda4893e30d61fe91e53cd1ed2132b
Alex Evans <alexevans@alexs-mbp.lan>
2020-01-03 21:44:22 -0600

    More progress on profile build - adding some Bootstrap and first project

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from projects.models import Project
 
 
-# Create your views here.
-def project_list(request):
-    return HttpResponse("<h1>Hello everyone!</h1>")
+def all_projects(request):
+    projects = Project.objects.all()
+
+    return render(request, 'projects/all_projects.html', {'projects': projects})
